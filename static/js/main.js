7f82daf6f324268f2bd6d04ff14406c0c390873f
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-09-11 00:37:18 -0300

    redirect handled in js

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,17 @@
-function triggerScript() {
-	fetch('/trigger-script', {
-		method: 'POST'
-	}).then(response => {
-		if (response.ok) {
-			alert('Script executed successfully');
-		} else {
-			alert('Failed to execute script');
-		}
-	}).catch(error => {
-		alert('Error: ' + error.message);
-	});
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById('trigger-button');
+    
+    button.addEventListener('click', function() {
+        fetch('/trigger-script', {
+            method: 'POST'
+        }).then(response => {
+            if (response.redirected) {
+                window.location.href = response.url;
+            } else {
+                alert('Failed to execute script');
+            }
+        }).catch(error => {
+            alert('Error: ' + error.message);
+        });
+    });
+});
