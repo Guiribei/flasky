ed521116a993d0dc39f79537e5e88609d92bb443
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-09-11 00:00:59 -0300

    add script.py to simulate json output

---
+++ script.py
@@ -0,0 +1,17 @@
+import json
+import time
+
+def generate_json():
+    data = {
+        "users": [
+            {"id": 1, "name": "Alice", "age": 25},
+            {"id": 2, "name": "Bob", "age": 30},
+            {"id": 3, "name": "Charlie", "age": 35}
+        ]
+    }
+    time.sleep(2)
+    
+    return json.dumps(data)
+
+if __name__ == "__main__":
+    print(generate_json())
